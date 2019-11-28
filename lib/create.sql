913524d31276aa73d4f4d40188b0aae1ce217932
Baruch Phillips <baruchphillips@gmail.com>
2019-11-28 16:05:33 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+        name TEXT, 
+        age integer, 
+        gender text,
+        color text, 
+        temperament text,
+        alive boolean
+);
+
+
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr.Chocolate',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mellisa',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch',7,'Male','brown','happy','yes') ; 
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy',7,'Male','brown','happy','yes') ;
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,7,'Male','brown','happy','yes') ;
+
+
